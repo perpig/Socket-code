33c94d86b39836ec86d7d16c5e009d32d30f08c3
perpig <1350575461@qq.com>
2024-10-15 08:05:57 +0800

    一些无关紧要的更改 在实现creator整个开发之后

--- QtCreator/server/server.h
+++ QtCreator/server/server.h
@@ -6,34 +6,37 @@
 #include <QTcpSocket>
 #include <QTcpServer>
 #include <QHostAddress> //主机地址 ，ip等
-#include <QSet> //存储连接的客户端ip
+#include <QSet>         //存储连接的客户端ip
 #include <QHash>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class server; }
+namespace Ui
+{
+    class server;
+}
 QT_END_NAMESPACE
 
-
-struct ID_IP {
-    QString ip;
+struct ID_IP
+{
+    QString ip; // 用户名
     QString id;
-    QTcpSocket *socket;  // 用于唯一标识客户端的指针
+    QTcpSocket *socket; // 用于唯一标识客户端的指针
 
     ID_IP(QString tid, QString tip, QTcpSocket *tsocket)
         : id(tid), ip(tip), socket(tsocket) {}
 
     // 重载==操作符
-    bool operator==(const ID_IP &other) const {
+    bool operator==(const ID_IP &other) const
+    {
         return id == other.id && ip == other.ip && socket == other.socket;
     }
 };
 
 // 为 ID_IP 类型提供 qHash 函数特化
-inline uint qHash(const ID_IP &key, uint seed = 0) {
+inline uint qHash(const ID_IP &key, uint seed = 0)
+{
     return qHash(key.id, seed) ^ qHash(key.ip, seed);
 }
-
-
 
 class server : public QWidget
 {
